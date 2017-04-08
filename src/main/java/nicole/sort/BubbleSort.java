4097fbc7953f303fadd48fe902a963818680967f
nicoleljc1227 <1170953465@qq.com>
2017-04-08 10:41:20 +0800

    冒泡排序

---
+++ src/main/java/nicole/sort/BubbleSort.java
@@ -0,0 +1,19 @@
+package sort;
+
+/**
+ * @Description:<p>冒泡排序</p>
+ * Created by xl on 2017/4/8.
+ */
+public class BubbleSort {
+    public static void bubbleSort(int [] array){
+        if(array == null || array.length == 0){
+            return;
+        }
+        for (int i = 0; i < array.length-1; i++) {
+            for (int j = 0; j < ; j++) {
+                
+            }
+        }
+    }
+
+}
